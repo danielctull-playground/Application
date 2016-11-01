5ac6c7cd2c8027366f05e25d6c6217265bbe90e3
Daniel Tull <dt@danieltull.co.uk>
2016-11-01 09:21:11 +0000

    Remove boilerplate code

--- Application/ViewController.swift
+++ Application/ViewController.swift
@@ -1,25 +1,5 @@
-//
-//  ViewController.swift
-//  Application
-//
-//  Created by Daniel Tull on 01/11/2016.
-//  Copyright © 2016 Daniel Tull. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
-
