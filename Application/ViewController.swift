f0dfad98534c4d283bb727607493760c96c97022
Daniel Tull <dt@danieltull.co.uk>
2016-11-01 09:42:28 +0000

    Show the value of Foo from the Framework

--- Application/ViewController.swift
+++ Application/ViewController.swift
@@ -1,5 +1,13 @@
 
 import UIKit
+import Framework
 
 class ViewController: UIViewController {
+
+	@IBOutlet var label: UILabel? {
+		didSet {
+			let value = Foo.value()
+			label?.text = String(value)
+		}
+	}
 }
